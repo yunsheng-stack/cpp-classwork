de3993c644fabca7e3618f6c96ab928475534124
yunsheng-stack <yunsheng45676@gmail.com>
2026-05-15 11:25:46 +0800

    Create variable.cpp

---
+++ variable.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int age;
+	string name;
+	cout << "請輸入年齡" << endl;
+	cin >> age;
+	cout << "請輸入名字" << endl;
+	cin >> name;
+	cout << "你是"<< name << "今年" << age << "歲" << endl; 
+}
