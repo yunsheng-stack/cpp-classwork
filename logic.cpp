08d6f2980cce8f3d9dc463f9e1aba89358f50c07
yunsheng-stack <yunsheng45676@gmail.com>
2026-05-22 11:40:00 +0800

    Create logic.cpp

---
+++ logic.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x1 = 5 + 3;
+    int x2 = 5 - 3;
+    int x3 = 5 * 3;
+    int x4 = 5 / 3;
+    int x5 = 5 % 3;
+    float x6 = 5 / 3.0;
+
+
+    cout << "x1 (5+3)  = " << x1 << endl;
+    cout << "x2 (5-3)  = " << x2 << endl;
+    cout << "x3 (5*3)  = " << x3 << endl;
+    cout << "x4 (5/3)  = " << x4 << endl;
+    cout << "x5 (5%3)  = " << x5 << endl;
+    cout << "x6 (5/3.0)= " << x6 << endl;
+
+    return 0;
+}
