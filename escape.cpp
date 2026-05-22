11d8306518ff490f4081f3c35d64c55a906d81c6
yunsheng-stack <yunsheng45676@gmail.com>
2026-05-22 11:27:45 +0800

    Create escape.cpp

---
+++ escape.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "1.快樂星球" << endl;
+	cout << "2.快樂 星球" << endl;
+	cout << "3.快樂\t星球" << endl;
+	cout << "4.快樂\n星球" << endl;
+	cout << "\"5.快樂星球\"" << endl;
+	cout << "\'6.快樂星球\'" << endl;
+	cout << "\\7.快樂星球\\" << endl;
+	return 0; 
+} 
